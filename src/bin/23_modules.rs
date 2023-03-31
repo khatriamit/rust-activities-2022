eb8ce1bf1d01ec71f4d8bb76b3bf6b5e12043a4b
khatriamit <khatriamit054@gmail.com>
2023-03-31 18:53:09 +0545

    [modules] modules info

---
+++ src/bin/23_modules.rs
@@ -0,0 +1,16 @@
+// ------------------------------------------------------
+//     - Rust Module
+//          - Binary crates: can execute
+//          - Library crate: can be used as module for other crate (binary)
+// ------------------------------------------------------
+fn main() {
+    /*
+        file is individual crate
+        modules are also the individual file
+            but modules are lower in hierarchy than crates
+            individual crate can include multiple modules
+                ex: lib.rs
+                    pub mod file_1;
+                    pub mod file_2;
+    */
+}
